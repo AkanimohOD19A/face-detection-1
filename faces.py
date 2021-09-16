a4350c9571caa6da8e44fc2dddfb4a92a1d87b7a
Daniel AMAH <daniel.amah@getsurveyplus.com>
2021-09-16 18:03:08 +0100

    Second Commit-Border-Frontal-Face

--- faces.py
+++ faces.py
@@ -1,11 +1,33 @@
 import numpy as np
 import cv2
 
+# Detect face
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+
 cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+    for (x,y,w,h) in faces:
+        # print(x,y,w,h)
+        # Greyscale region of interest
+        roi_gray = gray[y:y+h, x:x+w]
+
+        # Recogniser - Using DL
+
+        # img_item = "my-image.png"
+        # cv2.imwrite(img_item, roi_gray)
+
+        # Border ROI
+        color = (255, 0, 0)
+        stroke = 5
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color)
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
